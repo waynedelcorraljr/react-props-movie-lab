3041e67e9b496bb4f12045298c20ddc5b79304a1
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-04-02 22:32:31 -0500

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 export default class CardFront extends Component {
 
   render() {
+    // console.log(this.props)
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.style})`}}>
       </div>
     )
   }
 }
+
+
